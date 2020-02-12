af9f60551b6eb358f1a73dda4030b22b108f0d90
goljavi <somossedentarios@gmail.com>
2020-02-12 18:29:38 -0300

    Now Checkpoint has some visual feedback

--- Assets/Framework/Scripts/Checkpoint.cs
+++ Assets/Framework/Scripts/Checkpoint.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
+    bool activated;
 
 	void OnTriggerEnter(Collider c)
     {
-        if (c.gameObject.layer == Constantes.LAYER_PLAYER)
+        if (!activated && c.gameObject.layer == Constantes.LAYER_PLAYER)
         {
             GameManager.instance.checkPoint = transform.position;
             GameManager.instance.hasCheckpoint = true;
+            transform.GetChild(0).GetComponent<MeshRenderer>().materials[1].SetFloat("_RimPower", 0.1f);
+            activated = true;
         }
     }
 }
